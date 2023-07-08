f9855a53c09b5b9dd569e6c63634463b40325f4f
Mauricio Bonome <mauricio.bonome@hotmail.com>
2023-07-08 18:22:03 -0300

    Se agrego script en base.html para solucionar las imagenes

--- alquileres/settings.py
+++ alquileres/settings.py
@@ -144,7 +144,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
